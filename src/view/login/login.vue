b35275022d3f56bced76b2471db6045eb8c510fb
桔子 <juzi214032@qq.com>
2020-07-24 11:39:26 +0800

    refactor: 去除登录页面 logo

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login">
-    <div class="team-name hidden-sm-and-down"><img src="@/assets/image/login/team-name.png" alt="logo" /></div>
+    <!--<div class="team-name hidden-sm-and-down"><img src="@/assets/image/login/team-name.png" alt="logo" /></div>-->
     <div class="form-box" v-loading="loading" element-loading-background="rgba(0, 0, 0, 0)">
       <div class="title"><h1 title="Lin">OERP 在线考试报名平台</h1></div>
       <form class="login-form" autocomplete="off" @submit.prevent="throttleLogin()">
